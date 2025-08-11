24f835a9b0e815bac48d62478d4e465714451971
Adam Rackis <adamrackis@hotmail.com>
2025-08-10 23:52:33 -0500

    work

--- src/serverFn/middleware.ts
+++ src/serverFn/middleware.ts
@@ -2,24 +2,40 @@ import { createMiddleware } from "@tanstack/react-start";
 
 import { addLog } from "@/queries/logging";
 
-export const loggingMiddleware = (name: string) =>
+export const loggingMiddlewarePre = (name: string) =>
   createMiddleware({ type: "function" })
     .client(async ({ next, context }) => {
       console.log(name, "client", context);
 
-      return next({
+      const xxx = await next({
         sendContext: {
           traceId: crypto.randomUUID(),
           clientStart: new Date().toISOString()
         }
       });
+
+      console.log("after next", xxx);
+
+      return xxx;
     })
     .server(async ({ next, context }) => {
+      console.log("Server A");
+      await new Promise(resolve => setTimeout(resolve, 1000 * 1));
       const start = +new Date();
-      const result = await next({});
+      const result = await next({ sendContext: { message: "Success" } });
       const end = +new Date();
 
-      addLog(name, context.clientStart, context.traceId, end - start);
+      //addLog(name, context.clientStart, context.traceId, end - start);
+
+      return result;
+    });
+
+export const loggingMiddleware = (name: string) =>
+  createMiddleware({ type: "function" })
+    .middleware([loggingMiddlewarePre(name)])
+    .client(async ({ next, context }) => {
+      const result = await next({});
+      console.log("post hopefully", result);
 
       return result;
     });
